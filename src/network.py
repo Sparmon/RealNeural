d00f5ad05d6730dd2a8543d5bb619e6d128b0619
Ethan <Epweil1282@gmail.com>
2021-01-15 12:08:11 -0500

    Ways of detemrinign weights fixed

--- src/network.py
+++ src/network.py
@@ -1,5 +1,6 @@
 import torch
 from numpy import exp, array, random, dot
+import pandas as pd 
 
 
 class NeuralNetwork():
@@ -11,7 +12,9 @@ class NeuralNetwork():
         # We model a single neuron, with 3 input connections and 1 output connection.
         # We assign random weights to a 3 x 1 matrix, with values in the range -1 to 1
         # and mean 0.
-        self.synaptic_weights = 2 * random.random((3, 1)) - 1
+        self.synaptic_weights = pd.read_csv(("/Users/ethan/Desktop/Code with spencer /weights.csv") 
+        print("weights"),
+        print(self.synaptic_weights)
 
     # The Sigmoid function, which describes an S shaped curve.
     # We pass the weighted sum of the inputs through this function to
