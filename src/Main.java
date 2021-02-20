933867512bc5f70b2862923f870f03f20ed234bd
Sparmon <74984179+Sparmon@users.noreply.github.com>
2021-02-20 18:20:50 -0500

    Somewhat finished
    Needs minor improvements

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,15 @@ public class Main {
             {0,1},
             {1,1},
     };
-    static double[] Y= {0,1,0,1};
+    static double[] Y= {0,0,1,1};
 
     public static void main(String[] args) {
-        Trainer test = new Trainer(X,Y,10,0.1);
-        NeuralNetwork poop = new NeuralNetwork(2,3,1);
+        Trainer test = new Trainer(X,Y,100000,1);
+        NeuralNetwork poop = new NeuralNetwork(2,4,1);
         test.train(poop);
+//        double[] guess = {1,1,1};
+//        System.out.println();
+//        System.out.println(poop.predict(guess));
 
 
 
--- src/Trainer.java
+++ src/Trainer.java
@@ -14,31 +14,35 @@ public class Trainer {
 
     public void train(NeuralNetwork poop){
         for(int i = 0; i < time; i++) {
-            //THESE DON'T WORK BECAUSE I SOMETIMES DIVIDE BY 0!!!!
             Matrix bias_oGrad = bias_oGradCalc(poop);
-
             Matrix weights_hoGrad = weights_hoGradCalc(poop);
-
             Matrix bias_hGrad = bias_hGradCalc(poop);
-
             Matrix weights_ihGrad = weights_ihGradCalc(poop);
 
             poop.bias_o.add(bias_oGrad);
-            System.out.println("1");
-            bias_oGrad.print();
+//            System.out.println("1");
+//            bias_oGrad.print();
             poop.weights_ho.add(weights_hoGrad);
-            System.out.println("2");
-            weights_hoGrad.print();
+//            System.out.println("2");
+//            weights_hoGrad.print();
             poop.bias_h.add(bias_hGrad);
-            System.out.println("3");
-            bias_hGrad.print();
+//            System.out.println("3");
+//            bias_hGrad.print();
             poop.weights_ih.add(weights_ihGrad);
-            System.out.println("4");
-            weights_ihGrad.print();
-            System.out.println("5");
-            System.out.println(poop.predict(inp[0])+" " + poop.predict(inp[1])+" " +poop.predict(inp[2])+" " +poop.predict(inp[3]));
-        }
+//            System.out.println("4");
+//            weights_ihGrad.print();
+//            System.out.println("5");
 
+
+        }
+        print(poop);
+    }
+
+    public void print(NeuralNetwork poop){
+        System.out.println();
+        for(int i = 0; i < inp.length; i++){
+            System.out.print(poop.predict(inp[i]) + " ");
+        }
     }
 
     public Matrix weights_ihGradCalc(NeuralNetwork poop){
@@ -47,74 +51,56 @@ public class Trainer {
             Matrix in = Matrix.fromArray(inp[i]);
             Matrix inpu = Matrix.transpose(in);
             Matrix dsig = poop.zLm1(inp[i]).dsigmoid();
-            Matrix ret = Matrix.multiply(dsig, inpu);
             Matrix temp = hiddenGradCalc(poop);
-            temp.multiply(lrate);
-            for(int j = 0; j < ret.rows; j++){
-                for(int k = 0; k < ret.cols; k++){
-                        ret.data[j][k] = temp.data[j][0] / ret.data[j][k];
-
-                }
-            }
+            temp.multiply(lrate*2);
+            dsig.multiply(temp);
+            Matrix ret = Matrix.multiply(dsig, inpu);
             listy[i] = ret;
         }
         return (avgMatrix(listy));
     }
 
     public Matrix bias_hGradCalc(NeuralNetwork poop){
-        Matrix[] listy = new Matrix[4];
-        for(int i = 0; i < 4; i++){
+        Matrix[] listy = new Matrix[inp.length];
+        for(int i = 0; i < inp.length; i++){
             Matrix dsig = poop.zLm1(inp[i]).dsigmoid();
-            dsig.multiply(lrate);
-            for(int j = 0; j < dsig.rows; j++){
-                for(int k = 0; k < dsig.cols; k++){
-                        dsig.data[j][k] = hiddenGradCalc(poop).data[j][k] / dsig.data[j][k];
-                }
-            }
+            dsig.multiply(hiddenGradCalc(poop));
+            dsig.multiply(2*lrate);
             listy[i] = dsig;
         }
         return (avgMatrix(listy));
     }
 
     public Matrix hiddenGradCalc(NeuralNetwork poop){
-        Matrix[] listy = new Matrix[4];
-        for(int i = 0; i < 4; i++) {
+        Matrix[] listy = new Matrix[inp.length];
+        for(int i = 0; i <inp.length; i++) {
             Matrix weighty = Matrix.transpose(poop.weights_ho);
             Matrix ret = Matrix.multiply(weighty, poop.zL(inp[i]).dsigmoid());
             double temp = out[i] - poop.predict(inp[i]);
-            for(int j = 0; j < ret.rows; j++){
-                for(int k = 0; k < ret.cols; k++){
-                    ret.data[j][k] = temp/ret.data[j][k];
-                }
-            }
+            ret.multiply(temp * 2 * lrate);
             listy[i] = ret;
         }
         return avgMatrix(listy);
     }
 
     public Matrix weights_hoGradCalc(NeuralNetwork poop){
-        Matrix[] listy = new Matrix[4];
-        for(int i = 0; i < 4; i++){
+        Matrix[] listy = new Matrix[inp.length];
+        for(int i = 0; i < inp.length; i++){
             Matrix alm1 = Matrix.transpose(poop.aLm1(inp[i]));
+            double temp = out[i] - poop.predict(inp[i]);
+            alm1.multiply(2*lrate*temp);
             Matrix dsig = poop.zL(inp[i]).dsigmoid();
             Matrix ret = Matrix.multiply(dsig, alm1);
-            double temp = out[i] - poop.predict(inp[i]);
-            temp*=lrate;
-            for(int j = 0; j < ret.rows; j++){
-                for(int k = 0; k < ret.cols; k++){
-                    ret.data[j][k] = temp/ret.data[j][k];
-                }
-            }
             listy[i] = ret;
         }
         return (avgMatrix(listy));
     }
 
     public Matrix bias_oGradCalc(NeuralNetwork poop){
-        Matrix[] listy = new Matrix[4];
-        for(int i = 0; i < 4; i++){
+        Matrix[] listy = new Matrix[inp.length];
+        for(int i = 0; i < inp.length; i++){
             Matrix dsig = poop.zL(inp[i]).dsigmoid();
-            dsig.data[0][0] = ((out[i] - poop.predict(inp[i]))*lrate)/dsig.data[0][0];
+            dsig.data[0][0] = ((out[i] - poop.predict(inp[i]))*lrate*2);
             listy[i] = dsig;
         }
         return (avgMatrix(listy));
